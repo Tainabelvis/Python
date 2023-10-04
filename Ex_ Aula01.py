810435fab5afb6d6d571f792c414ebff4ae3fbde
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-04 07:21:11 -0300

    Delete Ex_ Aula01.py

--- Ex_ Aula01.py
+++
@@ -1,9 +0,0 @@
-Exercio 1
-
-nome=input('Qual é o seu nome?')
-print('Olá',nome, '!',' Prazer em te conhecer!')
-
-dia=input('Qual dia você nasceu?')
-mes=input('O mês do seu nascimento?')
-ano=input('O ano em que você nasceu?')
-print('Você nasceu no dia',dia,'de',mes,'de',ano,'.Correto?')
