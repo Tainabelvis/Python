2b91e9a49f3fd71234a26ab96669a86dc07bc06c
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 20:47:09 -0300

    Create Ex_05.py

---
+++ Estudos Python/Ex_05.py
@@ -0,0 +1,7 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+n = int(input("Digite um numero:"))
+d = n * 2
+t = n *3
+r = n ** (1/2)
+print("O dobro do numero digitado é: {} \nO triplo do numero digitado é: {} \nA raiz quadra do nunmero digitado é:{}" .format(d,t,r))
