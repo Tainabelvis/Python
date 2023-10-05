fd870b75d0d45ae48031e507094c8b6e3ee1bedc
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 20:33:03 -0300

    Update Ex_04.py

--- Estudos Python/Ex_04.py
+++ Estudos Python/Ex_04.py
@@ -1,2 +1,14 @@
 Operadores Aritméticos
 
+n1 = int(input("Um valor:"))
+n2 = int(input("Outro valor:"))
+s = n1 + n2
+su = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print("A soma é: {}, a subtração é: {}, a divisão é: {}, a multiplicão é: {}".format(s,su,d,m))
+print("A divisão inteira: {} e a potencia: {}". format(di,e))
+print("A soma é: {}, \nO potencial é: {}". format(s,e))
