5a7bc80c33a82a5305e22bc8a73de6ed2fedb207
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 18:49:43 -0300

    Update Ex_03_2.py

--- Estudos Python/Ex_03_2.py
+++ Estudos Python/Ex_03_2.py
@@ -1,1 +1,11 @@
+Variavel
 
+a = input('Digite algo:')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços?', a.isspace())
+print('É um numero?', a.isnumeric())
+print('É uma letra?', a.isalpha())
+print('É uma alfanumero', a.isalnum())
+print('Estão em maiúsculas?', a.isupper())
+print('Estão em minúsculas?', a.islower())
+print('Estão em maiúsculas e minúsculas?', a.istitle())
