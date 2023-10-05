ba341a2cf62c345c777a49bfbec3235eef3389e6
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 20:59:09 -0300

    Update Ex_06.py

--- Estudos Python/Ex_06.py
+++ Estudos Python/Ex_06.py
@@ -1,1 +1,6 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos Dólares ela pode comprar. Considere U$$1.00 = R$3.27
 
+d = int(input("Quanto de dinheiro você tem na carteira?"))
+dolar = d * 3.27
+
+print("Você consegue comprar U$$ {} dolares" .format(dolar))
