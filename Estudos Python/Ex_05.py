04ca47cf5a71c5a25734d1a3a26812b47aff19fa
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 21:08:45 -0300

    Update Ex_05.py

--- Estudos Python/Ex_05.py
+++ Estudos Python/Ex_05.py
@@ -2,6 +2,6 @@
 
 n = int(input("Digite um numero:"))
 d = n * 2
-t = n *3
+t = n * 3
 r = n ** (1/2)
 print("O dobro do numero digitado é: {} \nO triplo do numero digitado é: {} \nA raiz quadra do nunmero digitado é:{}" .format(d,t,r))
