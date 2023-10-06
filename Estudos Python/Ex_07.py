cc3fbf280ca016bdfdb69165a37ad1b10424dabe
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 21:07:37 -0300

    Create Ex_07.py

---
+++ Estudos Python/Ex_07.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu anteceor.
+
+n = int(input("Digite um número:"))
+a = n - 1
+s = n + 1
+
+print("Seu número é: {}, o sucessor é: {} e o antecessor é: {}" .format(n,a,s))
