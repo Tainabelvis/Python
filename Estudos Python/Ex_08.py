ac006f1d098a2fcb858e593b57ab4cf063977961
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-05 21:15:55 -0300

    Update Ex_08.py

--- Estudos Python/Ex_08.py
+++ Estudos Python/Ex_08.py
@@ -1,1 +1,8 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 
+n1 = float(input("Digite a primeira nota do aluno:"))
+n2 = float(input("Digite a segunda nota do aluno:"))
+
+notafinal = (n1 + n2) / 2
+
+print("A média final do aluno é: {}" .format(notafinal))
