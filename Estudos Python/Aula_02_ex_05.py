1adefcd102a611487001257f7d2e44ddaf8c5f1b
Tainá Belvis Garcez <127426024+Tainabelvis@users.noreply.github.com>
2023-10-30 06:40:33 -0300

    Update Aula_02_ex_05.py

--- Estudos Python/Aula_02_ex_05.py
+++ Estudos Python/Aula_02_ex_05.py
@@ -5,4 +5,7 @@ n1= str(input("Primeiro aluno:"))
 n2= str(input("Segundo aluno:"))
 n3= str(input("Terceiro aluno:"))
 n4= str(input("Quarto aluno:"))
-lista
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print("A ordem do sorterio sera:")
+print(lista)
